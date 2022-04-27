c27bf38f5be1d4b68e05d060fe6b51475cb83c34
Natacha De la Rosa <delarosa.natacha@gmail.com>
2022-04-27 19:36:03 -0400

    feat(table): add general table component

---
+++ src/common/components/FormatBadge.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+
+import { FormatType } from "common/types/types";
+
+interface Props {
+  type: FormatType;
+}
+const FormatBadge: FC<Props> = ({ type }) => {
+  let backgroundFormat;
+  switch (type) {
+    case FormatType.pdf:
+      backgroundFormat = "background-pdf";
+      break;
+    case FormatType.csv:
+      backgroundFormat = "background-csv";
+      break;
+    case FormatType.xls:
+    case FormatType.xlsx:
+      backgroundFormat = "background-xls";
+      break;
+    case FormatType.ods:
+      backgroundFormat = "background-ods";
+      break;
+    case FormatType.other:
+      backgroundFormat = "background-other";
+      break;
+  }
+
+  return <span className={`badge ${backgroundFormat}`}>{type}</span>;
+};
+
+export default FormatBadge;
---
+++ src/common/types/types.ts
@@ -0,0 +1,79 @@
+/* eslint-disable camelcase */
+
+export enum StatusType {
+  under_review = "under_review",
+  on_line = "on_line",
+  blocked = "blocked",
+  broken = "broken",
+}
+
+export enum FormatType {
+  csv = "csv",
+  xls = "xls",
+  xlsx = "xlsx",
+  pdf = "pdf",
+  ods = "ods",
+  other = "other",
+}
+
+export interface Aletheia {
+  id: string;
+  proof: {
+    id: string;
+    url: string;
+  };
+}
+
+export interface File {
+  id: string;
+  url: string;
+}
+
+export interface Department {
+  id: string;
+  name: string;
+  description: string;
+  website: string;
+}
+
+export interface Category {
+  id: string;
+  title: string;
+}
+
+export interface Source {
+  url: string;
+  id: string;
+  name: string;
+}
+
+export interface Alexandrias {
+  title: string;
+  description: string;
+  status: string;
+  cid: string;
+  type: string;
+  aletheias: [Aletheia];
+}
+
+export interface Record {
+  id: string;
+  cid: string;
+  description: string;
+  status: StatusType;
+  title: string;
+  name?: string;
+  updatedAt?: string;
+  type: FormatType;
+  aletheias: [Aletheia];
+  alexandrias: [Alexandrias];
+  file: [File];
+  departament: Department;
+  category: Category;
+  api_endpoint: string;
+  source_url: string;
+  wallet_address: string;
+  source: Source;
+  website: string;
+  url: string;
+}
